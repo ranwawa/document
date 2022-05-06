ecc890ba3ecf924292d0fa30bb88d416c686b7f0
ranwawa <macbookpro@macbooks-MacBook-Pro.local>
2022-05-06 12:37:12 +0800

    build: 🎡 新增treelint配置y文件

---
+++ .treelintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  '.git': 'directory',
+  '.gitignore': 'file',
+  _media: 'directory',
+  '.husky': {
+    _: 'directory',
+    'pre-push': 'file',
+  },
+  '^[-._a-zA-Z0-9]+$': 'regexp',
+};
